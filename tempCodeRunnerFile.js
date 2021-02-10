6bf016ae1741c040fd624e873eec1e0831bdcf87
Akash Kumar <65059360+amnegan@users.noreply.github.com>
2021-02-10 20:24:41 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-console.log('\nStarting the miner again...');
-// Negan.minePendingTransactions('xyz-address');
-// console.log('\nBalance :',Negan.getBalanceOfAddress('xyz-address'));
-
-// console.log('\nStarting the miner 3rd time...');
-// Negan.minePendingTransactions('xyz-address');
-// console.log('\nBalance :',Negan.getBalanceOfAddress('xyz-address'));
